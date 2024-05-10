a97dd4f33b97b0280fbf5bb9828ac8c3c1095918
Lu-cc <luca.var@gmail.com>
2024-05-10 11:59:56 +0000

    docker compose file modified to match ingest script

---
+++ terraform/variables.tf
@@ -0,0 +1,41 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "~/.gc/my-creds.json"
+  #ex: if you have a directory where this file is called keys with your service account json file
+  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+}
+
+
+variable "project" {
+  description = "Project"
+  default     = "nytaxidata-422211"
+}
+
+variable "region" {
+  description = "Region"
+  #Update the below to your desired region
+  default     = "europe-west1"
+}
+
+variable "location" {
+  description = "Project Location"
+  #Update the below to your desired location
+  default     = "europe-west1"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  #Update the below to what you want your dataset to be called
+  default     = "demo_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  #Update the below to a unique bucket name
+  default     = "terraform-ny-taxi-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
